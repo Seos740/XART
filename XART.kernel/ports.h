8a21cc111c9a041978b8b3b4fe97a2bd747cf9d7
Seos740 <dicksonstanley503@gmail.com>
2025-04-27 20:33:18 +0800

    Fixes a bug that would close all ports
    a bug exists in the previous revision that would cause all ports to close and the kernel to become very unstable

--- XART.kernel/ports.h
+++ XART.kernel/ports.h
@@ -14,3 +14,10 @@ int XART_ENABLE_PORT(LONG port_id) {
     XART_PORT_OPENER = 0x0F;
     XART_OPEN_PORT = port_id;
 }
+
+int RESET() {
+    XART_PORT_CLOSER = 0x00;
+    XART_CLOSE_PORT = 0x00;
+    XART_PORT_OPENER = 0x00;
+    XART_OPEN_PORT = 0x00;
+}
